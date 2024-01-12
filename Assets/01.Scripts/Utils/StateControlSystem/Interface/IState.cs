bea1c7ca16c27fe1d63adb449f8df59e2216b258
ehdbs28 <doyun5373@naver.com>
2024-01-12 15:47:43 +0900

    remove project

--- Assets/01.Scripts/Utils/StateControlSystem/Interface/IState.cs
+++
@@ -1,9 +0,0 @@
- namespace StateControlSystem
- {
-     public interface IState
-     {
-         public void EnterState();
-         public void UpdateState();
-         public void ExitState();
-     }
- }
