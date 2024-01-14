0ec9d09bf87bfb32be2fce458e7e934c55aeb53d
ehdbs28 <doyun5373@naver.com>
2024-01-14 18:52:21 +0900

    basic entity system

--- Assets/01.Scripts/Utils/EntitySystem/Entity.cs
+++ Assets/01.Scripts/Utils/EntitySystem/Entity.cs
@@ -6,6 +6,26 @@ public class Entity : MonoBehaviour
 {
     [SerializeField] private EntityData _data;
     public EntityData Data => _data;
+    
+    public Transform ModelTrm { get; private set; }
 
     public Rigidbody RigidbodyCompo { get; private set; }
+    public Animator AnimatorCompo { get; private set; }
+
+    public virtual void Awake()
+    {
+        ModelTrm = transform.Find("Model");
+        RigidbodyCompo = GetComponent<Rigidbody>();
+        AnimatorCompo = ModelTrm.GetComponent<Animator>();
+    }
+
+    public void SetVelocity(Vector3 velocity)
+    {
+        RigidbodyCompo.velocity = velocity;
+    }
+
+    public void AddForce(Vector3 force)
+    {
+        RigidbodyCompo.AddForce(force);
+    }
 }
