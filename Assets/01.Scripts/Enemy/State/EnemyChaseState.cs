7d2a89720a72fdbbb2c651732881ad6a500c8a46
ehdbs28 <doyun5373@naver.com>
2024-02-01 02:00:54 +0900

    enemy state setting

---
+++ Assets/01.Scripts/Enemy/State/EnemyChaseState.cs
@@ -0,0 +1,10 @@
+public class EnemyChaseState : State
+{
+    public EnemyChaseState(StateController controller, string animationParameter) : base(controller, animationParameter)
+    {
+    }
+
+    public override void UpdateState()
+    {
+    }
+}
---
+++ Assets/01.Scripts/Enemy/State/EnemyHitState.cs
@@ -0,0 +1,7 @@
+namespace _01.Scripts.Enemy.State
+{
+    public class EnemyHitState
+    {
+        
+    }
+}
