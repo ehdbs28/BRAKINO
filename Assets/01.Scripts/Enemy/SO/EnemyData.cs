d9a294a3fb9e07b2dc44abf8c059d010444f8165
ehdbs28 <doyun5373@naver.com>
2024-02-04 19:29:52 +0900

    nav mesh setting

--- Assets/01.Scripts/Enemy/SO/EnemyData.cs
+++ Assets/01.Scripts/Enemy/SO/EnemyData.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "SO/EntityData/EnemyData")]
 public class EnemyData : EntityData
 {
+    [Header("Enemy Sense Data")] 
+    public float senseRadius;
+    public float senseDelay;
+
     [Header("Enemy Attack Data")] 
     public float attackRadius;
     public float attackDistance;
